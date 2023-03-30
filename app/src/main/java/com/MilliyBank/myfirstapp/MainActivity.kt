b4f215ee6bc5f36b5086e8b9ae2495f3cae6cf2d
Alisher <101879695+Alisher19121994@users.noreply.github.com>
2023-03-30 18:35:07 +0500

    Initial commit

---
+++ app/src/main/java/com/MilliyBank/myfirstapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.MilliyBank.myfirstapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var button:AppCompatButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+
+    }
+    private fun initViews() {
+        button = findViewById(R.id.clickAppCompatButton_id)
+        button.setOnClickListener {
+            val openActivity = Intent(this@MainActivity,MainActivity2::class.java)
+            startActivity(openActivity)
+        }
+    }
+
+}
